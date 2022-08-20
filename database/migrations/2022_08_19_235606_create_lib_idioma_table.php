e65a32240d8cf29ff2dd319f721ec4281dc1b0a9
cristiancry <50432278+cristiancry@users.noreply.github.com>
2022-08-19 21:09:02 -0400

    migraciones agregadas

--- database/migrations/2022_08_19_235606_create_lib_idioma_table.php
+++ database/migrations/2022_08_19_235606_create_lib_idioma_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('lib_idioma', function (Blueprint $table) {
+            $table->increments('cod_idioma');
+            $table->string('descripcion',100)->unique();
         });
     }
 
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('lib_idioma');
     }
 };
